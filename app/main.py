6cac317da0a8f8caac4f543418b3719deb035846
Abdullah Kazmi <iamabdullah234@gmail.com>
2025-02-13 00:53:50 +0500

    Seeder Implemented

--- app/main.py
+++ app/main.py
@@ -1,12 +1,40 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
-
 from .database import engine, Base
+from .models import User
+from passlib.hash import bcrypt
+from sqlalchemy import event
+from app.user import user
 
 load_dotenv()
+
+INITIAL_DATA = {
+    "users": [
+        {
+            "id": 1,
+            "name": "admin",
+            "username": "admin",
+            "email": "admin@example.com",
+            "password": bcrypt.hash("admin123"),
+            "role": "admin",
+        }
+    ]
+}
+
+
+def initialize_table(target, connection, **kw):
+    tablename = str(target)
+    if tablename == "users" and len(INITIAL_DATA["users"]) > 0:
+        connection.execute(target.insert(), INITIAL_DATA["users"])
+
+
+event.listen(User.__table__, "after_create", initialize_table)
+
 app = FastAPI()
 
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+
