1a93ba85e37b5c9f4681fd27ee24a323b5d631a9
Waqas Ahsan <iamabdullah234@gmail.com>
2025-04-14 20:04:30 +0500

    fix: Http Exception

--- app/formdata/form.py
+++ app/formdata/form.py
@@ -85,3 +85,9 @@ def onboarding(
             },
             status_code=status.HTTP_201_CREATED,
         )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"An error occurred: {str(e)}",
+        )
